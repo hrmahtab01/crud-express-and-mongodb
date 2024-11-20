fd253df4b6228963ecdb2edb9a6f4cf5629f78c0
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-11-20 15:14:32 +0600

    add delete method

--- index.js
+++ index.js
@@ -50,9 +50,15 @@ app.put("/api/product/:id", async (req, res) => {
 });
 app.delete("/api/product/:id", async (req, res) => {
   try {
+    const { id } = req.params;
+    const products = await Product.findByIdAndDelete(id, req.body);
+    res.status(200).json("product delete successfully");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+});
+app.use((req, res) => {
+  res.status(404).send("404 not found");
 });
 
 app.listen(3030, () => {
